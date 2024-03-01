bc8b297478abe2d32e1816ac01ada65de7e80092
Julian Michel <mail@julianmichel.de>
2024-03-01 22:37:15 +0100

    initial pipeline

---
+++ lib/cdk-pipeline-v2-app-stack.ts
@@ -0,0 +1,14 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class CdkPipelineV2AppStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // example resource
+    const queue = new sqs.Queue(this, 'CdkPipelineV2Queue', {
+      visibilityTimeout: cdk.Duration.seconds(300)
+    });
+  }
+}
---
+++ lib/cdk-pipeline-v2-pipeline-stack.ts
@@ -0,0 +1,38 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as pipelines from 'aws-cdk-lib/pipelines';
+
+export class CdkPipelineV2PipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
+      synth: new pipelines.ShellStep('Synth', {
+        input: pipelines.CodePipelineSource.connection('jumic/cdk-pipeline-v2', 'main', {
+          connectionArn: 'arn:aws:codestar-connections:eu-west-1:857739166276:connection/fa150468-a35d-4b7c-a9e5-bf510e60f0d0',
+        }),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth',
+        ],
+      }),
+    });
+
+    pipeline.addStage(new MyApplication(this, 'Prod', {
+      env: {
+        account: '857739166276',
+        region: 'eu-west-1',
+      },
+    }));
+
+  }
+}
+
+class MyApplication extends cdk.Stage {
+  constructor(scope: Construct, id: string, props?: cdk.StageProps) {
+    super(scope, id, props);
+
+    new CdkPipelineV2PipelineStack(this, 'CdkPipelineV2AppStack');
+  }
+}
