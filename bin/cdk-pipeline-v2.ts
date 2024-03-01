6a20ae1886ced6ee67a45b9ae565accc669caba7
Julian Michel <mail@julianmichel.de>
2024-03-01 22:37:55 +0100

    rename stacks

--- bin/cdk-pipeline-v2.ts
+++ bin/cdk-pipeline-v2.ts
@@ -5,10 +5,10 @@ import { CdkPipelineV2AppStack } from '../lib/cdk-pipeline-v2-app-stack';
 import { CdkPipelineV2PipelineStack } from '../lib/cdk-pipeline-v2-pipeline-stack';
 
 const app = new cdk.App();
-new CdkPipelineV2AppStack(app, 'CdkPipelineV2Stack', {
+new CdkPipelineV2AppStack(app, 'CdkPipelineV2AppStack', {
   env: { account: '857739166276', region: 'eu-west-1' },
 });
 
-new CdkPipelineV2PipelineStack(app, 'CdkPipelineV2Stack', {
+new CdkPipelineV2PipelineStack(app, 'CdkPipelineV2PipelineStack', {
   env: { account: '857739166276', region: 'eu-west-1' },
 });
